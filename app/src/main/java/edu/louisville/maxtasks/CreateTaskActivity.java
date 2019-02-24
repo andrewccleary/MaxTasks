7b2ea485100608f083480e28f314ecba32ed1e67
Andrew Cleary <acclea01@louisville.edu>
2019-02-23 19:09:42 -0500

    Added drawer navigation to main activity

---
+++ app/src/main/java/edu/louisville/maxtasks/CreateTaskActivity.java
@@ -0,0 +1,13 @@
+package edu.louisville.maxtasks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_task);
+    }
+}
